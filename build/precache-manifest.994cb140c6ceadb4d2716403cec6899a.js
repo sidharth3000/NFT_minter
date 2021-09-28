26ddbd97cb9924efb0fb42e225748a5d55b99651
sidharth3000 <201952234@iiitvadodara.ac.in>
2021-09-29 00:13:04 +0530

    finsihed

---
+++ build/precache-manifest.994cb140c6ceadb4d2716403cec6899a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d2bf6da4a73b2d3864f20170afc451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a291eeee7929f8ae0593",
+    "url": "/static/css/main.323a554d.chunk.css"
+  },
+  {
+    "revision": "b57d345295460a76eb6d",
+    "url": "/static/js/2.a6d61a77.chunk.js"
+  },
+  {
+    "revision": "a291eeee7929f8ae0593",
+    "url": "/static/js/main.8f5cfda2.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  },
+  {
+    "revision": "8cea1391b8e0717ea6532ebfa5cf4a34",
+    "url": "/static/media/twitter-logo.8cea1391.svg"
+  }
+]);
